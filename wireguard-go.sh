a4529b117d797c4d5b0c13b54d7fe523733de2e4
yirenchengfeng1 <148023915+yirenchengfeng1@users.noreply.github.com>
2024-07-27 12:06:14 +0800

    Update wireguard-go.sh

--- wireguard-go.sh
+++ wireguard-go.sh
@@ -77,15 +77,25 @@ echo -e "${INFO} Get ${PROJECT_NAME} download URL ..."
             echo -e "${INFO} 未能获取到有效的公网 IP 地址。"
             return 1
         else
-		    cat >> /etc/hosts << EOF
-# https://danwin1210.de/github-ipv6-proxy.php
-2a01:4f8:c010:d56::2 github.com
-2a01:4f8:c010:d56::3 api.github.com
-2a01:4f8:c010:d56::4 codeload.github.com
-2a01:4f8:c010:d56::5 objects.githubusercontent.com
-2a01:4f8:c010:d56::6 ghcr.io
-2a01:4f8:c010:d56::7 pkg.github.com npm.pkg.github.com maven.pkg.github.com nuget.pkg.github.com rubygems.pkg.github.com
-EOF
+        # 定义要添加的条目
+        entries=(
+"2a01:4f8:c010:d56::2 github.com"
+"2a01:4f8:c010:d56::3 api.github.com"
+"2a01:4f8:c010:d56::4 codeload.github.com"
+"2a01:4f8:c010:d56::5 objects.githubusercontent.com"
+"2a01:4f8:c010:d56::6 ghcr.io"
+"2a01:4f8:c010:d56::7 pkg.github.com npm.pkg.github.com maven.pkg.github.com nuget.pkg.github.com rubygems.pkg.github.com"
+)
+
+		# 检查并添加缺失的条目
+		for entry in "${entries[@]}"; do
+		  if ! grep -qF "$entry" /etc/hosts; then
+			echo "$entry" >> /etc/hosts
+			echo "Added: $entry"
+		  else
+			echo "Already exists: $entry"
+		  fi
+		done
             DOWNLOAD_URL=https://github.com/P3TERX/wireguard-go-builder/releases/download/0.0.20230223/wireguard-go-${FILE_KEYWORD}.tar.gz
         fi
     else
